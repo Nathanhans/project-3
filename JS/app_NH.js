4f5b61d632408a8e718b9d66276d25228804c976
Jonah Kopp <jonah.kopp123@gmail.com>
2024-01-25 21:41:56 -0700

    mapping working

--- JS/app_NH.js
+++ JS/app_NH.js
@@ -1,9 +1,33 @@
 // Fetch the JSON data
 
-let presResults = d3.json('../ETL/presidential_results.json');
+// let presResults = d3.json('../ETL/presidential_results.json');
 
-console.log(presResults)
+// console.log(presResults)
 
+
+let myMap = L.map("map", {
+    center: [36.7783, -119.4179],
+    // center:[40.7128, -74.0059],
+    zoom: 7
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // Use this link to get the GeoJSON data.
+//   let link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
+  
+  // Getting our GeoJSON data
+//   d3.json('../ETL/presidential_results.json').then(function(data) {
+  d3.json('../data/California_County_Boundaries.geojson').then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data
+
+    console.log(data)
+
+    L.geoJson(data).addTo(myMap);
+  });
 // {
   
 
