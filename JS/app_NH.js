e9f047840096ae51ad7ed5965bfc4173b3e47127
Nathanhans <nathan.hans@gmail.com>
2024-02-01 20:52:06 -0700

    final touches on formatting

--- JS/app_NH.js
+++ JS/app_NH.js
@@ -1,8 +1,5 @@
-// Fetch the JSON data
+//set initial drop down array values and initialize charts and map
 
-// let presResults = d3.json('../ETL/presidential_results.json');
-
-// console.log(presResults)
 
 let dropDownArray = [2000,2004,2008,2012,2016,2020];
 let electionResults;
@@ -58,11 +55,6 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
 
         // Attach election data to GeoJSON properties
         layer.feature.properties.electionResults = electionResults;
-
-        // Attaching election data to popup
-        // popup example
-        // layer.bindPopup("<strong>" + feature.properties.NAME + "</strong><br /><br />Estimated employed population with children age 6-17: " +
-        // feature.properties.DP03_16E + "<br /><br />Estimated Total Income and Benefits for Families: $" + feature.properties.DP03_75E);
 
         // Attaching election data to popup
         layer.bindPopup(`<h3><strong> County: </strong> ${electionResults[0].county_name}</h3>
@@ -128,8 +120,11 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
 //CHART 1, HORIZONTAL BAR
 let trace1 = {x: yVals, y: xVals, orientation:'h', type: 'bar', marker: {color: colors}};
 let data = [trace1];
-let layout = {title: 'Winning Percentage of Votes by County', font: {size: 15}, showlegend: false, 
-              xaxis: {title: {text: 'Percent of Total Votes'},  yaxis: {title: {text: 'County Names'}, automargin:true}}};
+let layout = {
+              title: {text: 'Winning Percentage of Votes by County', font: {size: 22}}, showlegend: false, 
+              // xaxis: {title: {text: ''}},  
+              yaxis: {title: {text: ''}, automargin: true}, font: {size:10} 
+             };
 
 Plotly.newPlot("bar", data, layout); 
 
@@ -139,34 +134,8 @@ var data2 = [{values: Object.values(party_votes), labels: Object.keys(party_vote
 var layout2 = {title: 'Percentage of Counties Won by Party', font: {size: 15}, showlegend: false};
 Plotly.newPlot('bubble', data2,layout2);
 
-
-    
-
-
-
-
-
-
-
-
-
-    // Initialize Pie Chart
-
-
-
-
-
-
-
-
-
-
-
     })
 })
-
-
-
 
 // Getting our GeoJSON data
 function optionChanged(dropdownValue) {
@@ -185,9 +154,7 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
 
     d3.json('../etl/presidential_results_only.json').then(function(electionData) 
     {
-
-        //THIS IS WHERE THE DROP DOWN GOES...LET'S CREATE AN ARRAY INSTEAD OF A LOOP
-
+        
       countiesLayer.eachLayer(function (layer)
       {
         
@@ -221,28 +188,7 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
         }
       })
 
-    // add legend WIP
-      // var legend = L.control({position: 'bottomleft'});
-      // legend.onAdd = function () {
-  
-      // var div = L.DomUtil.create('div', 'info legend');
-      // labels = ['<strong>Depth</strong>'],
-      // depthBuckets = getDepthBuckets(minDepth,maxDepth,numBuckets)
-  
-      // for (var i = 0; i < depthBuckets[0].length; i++) {
-  
-      //     labels.push("<li style=\"background-color: " + depthColor(depthBuckets[0][i],minDepth,maxDepth,numBuckets) + "\"></li>" + (depthBuckets[1][i] ? depthBuckets[1][i] : '+'));
-  
-      //     }
-  
-      //     div.innerHTML = labels.join('<br>');
-          
-      // return div;
-      // };
-      // legend.addTo(myMap);
-
-
-    // Update Bar Chart
+    // Loop for update on drop down
 
     var electionResults = electionData.filter(function(item) {
       // Currently filtering to just year 2000-- ideally this will by dynamic based on election drop down
@@ -280,10 +226,15 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
           
     }
       // UPDATE BAR CHART
+      
+
       let trace1 = {x: yVals, y: xVals, orientation:'h', type: 'bar', marker: {color: colors}};
       let data = [trace1];
-      let layout = {title: 'Winning Percentage of Votes by County', font: {size: 15}, showlegend: false, 
-              xaxis: {title: {text: 'Percent of Total Votes'},  yaxis: {title: {text: 'County Names'}, automargin:true}}};
+      let layout = {
+        title: {text: 'Winning Percentage of Votes by County', font: {size: 22}}, showlegend: false, 
+        // xaxis: {title: {text: ''}},  
+        yaxis: {title: {text: ''}, automargin: true}, font: {size:10} 
+       };
       
       Plotly.newPlot("bar", data, layout); 
       
@@ -293,13 +244,6 @@ d3.json('../data/California_County_Boundaries.geojson').then(function(data)
       var data2 = [{values: Object.values(party_votes), labels: Object.keys(party_votes), type: 'pie', marker: {colors: ['rgb(0, 48, 240)', 'rgb(215, 11, 11)']}}];
       var layout2 = {title: 'Percentage of Counties Won by Party', font: {size: 15}, showlegend: false};
       Plotly.newPlot('bubble', data2,layout2);
-
-
-
-
-
-
-
 
 
     })
@@ -318,89 +262,3 @@ function getColor(electionResults)
   }
 
 }
-
-// {
-  
-
-//   for (let i = 0; i < data.samples.length; i++)
-//   {
-//     d3.select("#selDataset").append("option").text(data.samples[i].id) //the drop down is referencing this text
-    
-//     let sample = {
-//         id: data.samples[i].id,
-//         sample_values: data.samples[i].sample_values,
-//         odu_ids: data.samples[i].otu_ids.map(i => 'OTU ' + i),
-//         odu_num: data.samples[i].otu_ids,
-//         odu_labels: data.samples[i].otu_labels
-//                 }
-//     samples.push(sample)
-
-//   } //end of samples loop
-
-//   //metadata loop
-//   for (let i = 0; i < data.metadata.length; i++)
-//   {
-    
-//     let meta = {
-//       id: data.metadata[i].id,
-//       ethnicity: data.metadata[i].ethnicity,
-//       gender: data.metadata[i].gender,
-//       age: data.metadata[i].age,
-//       location: data.metadata[i].location,
-//       bbtype: data.metadata[i].bbtype,
-//       wfreq: data.metadata[i].wfreq
-//     }
-//     metadata.push(meta)
-//   } //end of metadata loop
-// } //end of function
-// ;
-
-// //call getData function on change to dropdown
-    
-// function optionChanged(value) 
-// {
-
-//   for (let i = 0; i <samples.length; i++) //loop for plotly data
-//   {
-//      if (samples[i].id == value)
-//      {
-//        let vals = samples[i].sample_values.slice(0,10);
-//        let tot_vals = samples[i].sample_values;
-//        let odus = samples[i].odu_ids.slice(0,10);
-//        let tot_odus = samples[i].odu_num;
-//        console.log(samples[i]);
-
-// //CHART 1, HORIZONTAL BAR
-//       let trace1 = {x: vals, y: odus, type: 'bar', orientation: 'h',hoverinfo: odus };
-//       let data = [trace1];
-//       let layout = { };
-//       Plotly.newPlot("bar", data, layout);    
-
-// //CHART 2, BUBBLE
-
-//       let trace2 = {x: tot_odus,y: tot_vals, mode: 'markers', marker: { size: tot_vals,color: tot_odus, hoverinfo: odus} };
-//       let data2 = [trace2];
-
-//       Plotly.newPlot('bubble', data2);
-
-//      }
-//    }//end of loop
-  
-  
-//    for (let i = 0; i <metadata.length; i++) //loop for demographic info
-//    {
-//     if (metadata[i].id == value)
-//      {
-//       // d3.select('#sample-metadata').remove("option");
-//       d3.select("#sample-metadata").raise("option").text("ID: " + metadata[i].id); //id
-//       d3.select("#sample-metadata").append("option").text("Ethnicity: " + metadata[i].ethnicity); //ethnicity
-//       d3.select("#sample-metadata").append("option").text("Gender: " + metadata[i].gender); //gender
-//       d3.select("#sample-metadata").append("option").text("Age: " + metadata[i].age); //age
-//       d3.select("#sample-metadata").append("option").text("Location: " + metadata[i].location); //location
-//       d3.select("#sample-metadata").append("option").text("BBType: " + metadata[i].bbtype); //bbtype
-//       d3.select("#sample-metadata").append("option").text("WFreq: " + metadata[i].wfreq); //wfreq
-//      }
-//   }
-// }
-// ;
-
